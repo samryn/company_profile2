8131a82d017ca4aa7adca8290824857c15135b9a
samryn <mehmedhesham27@gmail.com>
2025-04-22 18:09:55 +0700

    update

--- index.php
+++ index.php
@@ -29,16 +29,19 @@
         <h2>Tentang Kami</h2>
         <p>PT. MITRATEC NUSANTARA DJAYA merupakan perusahaan yang bergerak di bidang pelaksanaan dan tergabung dalam Asosiasi AKLI.
           PT. MITRATEC NUSANTARA DJAYA berlokasi di KAB. TANAH BUMBU, Indonesia, tepatnya di Jl. Sampurna RT. 08 Desa Barokah - Kec Simpang Empat - Batulicin KAB. TANAH BUMBU, Indonesia.
-          PT. MITRATEC NUSANTARA DJAYA memiliki reputasi baik dalam memberikan layanan berkualitas tinggi di sektor konstruksi.</p>
+          PT. MITRATEC NUSANTARA DJAYA memiliki reputasi baik dalam memberikan layanan berkualitas tinggi di sektor konstruksi. Dengan bentuk badan usaha PT, PT. MITRATEC NUSANTARA DJAYA 
+          memiliki komitmen untuk memberikan hasil terbaik dalam setiap proyek yang dijalankan. Perusahaan ini senantiasa menjunjung tinggi profesionalisme dan kualitas sesuai dengan standar yang telah ditetapkan oleh asosiasi AKLI.</p>
     </section>
 
     <section id="catalog">
         <h2>Layanan</h2>
+        <p>PT. MITRATEC NUSANTARA DJAYA merupakan perusahaan yang bergerak di bidang usaha dengan jenis Pelaksanaan, 
+            dimana beberapa jasa konstruksi yang dikerjakan oleh PT. MITRATEC NUSANTARA DJAYA diantaranya:</p>
         <ul>
-            <li>Produk 1</li>
-            <li>Produk 2</li>
-            <li>Produk 3</li>
-            <li>Produk 4</li>
+            <li>Jasa Konstruksi EL002: Jasa Pelaksana Instalasi Pembangkit Tenaga Listrik daya maksimum 10 MW</li>
+            <li>Jasa Konstruksi EL006: Jasa Pelaksana Konstruksi Jaringan Distribusi Tenaga Listrik Tegangan Menengah</li>
+            <li>Jasa Konstruksi EL007: Jasa Pelaksana Konstruksi Instalasi Jaringan Distribusi Tenaga Listrik Tegangan Rendah</li>
+            <li>Jasa Konstruksi EL010: Jasa Pelaksana Instalasi Tenaga Listrik Gedung dan Pabrik</li>
         </ul>
     </section>
 
